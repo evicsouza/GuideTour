36201770d44c7f5c7cb19ed6fe59e901183e17b0
evicsouza <evavic.ifpe@gmail.com>
2022-10-27 13:38:07 -0300

    add fragment

--- app/src/main/java/com/evavictoria/locus/guidetour/MainActivity.java
+++ app/src/main/java/com/evavictoria/locus/guidetour/MainActivity.java
@@ -6,10 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.evavictoria.locus.guidetour.controller.OndeirActivity;
-import com.evavictoria.locus.guidetour.controller.RestaurantesActivity;
-import com.evavictoria.locus.guidetour.controller.RotasActivity;
-import com.evavictoria.locus.guidetour.controller.ServmedicosActivity;
 import com.evavictoria.locus.guidetour.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,46 +22,25 @@ public class MainActivity extends AppCompatActivity {
         binding.imageViewRotas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                botaoRotas(view);
             }
         });
         binding.imageViewOndeIr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                botaoOndeIr(view);
+
             }
         });
         binding.imageViewRestaurantes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                botaoRestaurantes(view);
+
             }
         });
         binding.imageViewServ2Medicos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                botaoServMedicos(view);
+
             }
         });
-    }
-
-    public void botaoRotas(View view) {
-        Intent intent = new Intent(MainActivity.this, RotasActivity.class);
-        startActivity(intent);
-    }
-
-    public void botaoRestaurantes(View view) {
-        Intent intent = new Intent(MainActivity.this, RestaurantesActivity.class);
-        startActivity(intent);
-    }
-
-    public void botaoOndeIr(View view) {
-        Intent intent = new Intent(MainActivity.this, OndeirActivity.class);
-        startActivity(intent);
-    }
-
-    public void botaoServMedicos(View view) {
-        Intent intent = new Intent(MainActivity.this, ServmedicosActivity.class);
-        startActivity(intent);
     }
 }
