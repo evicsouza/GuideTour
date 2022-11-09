c116e8b32e2b15121ba41f9f2664714ceacd10cd
evicsouza <evavic.ifpe@gmail.com>
2022-11-09 14:15:49 -0300

    criação do gráfico de navegação

--- app/src/main/java/com/evavictoria/locus/guidetour/MainFragment.java
+++ app/src/main/java/com/evavictoria/locus/guidetour/MainFragment.java
@@ -1,19 +1,26 @@
 package com.evavictoria.locus.guidetour;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
-
-import com.evavictoria.locus.guidetour.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+   @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
 }
