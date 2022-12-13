4d927f2ee210cfceaa5a6b87e798734b89658657
evicsouza <evavic.ifpe@gmail.com>
2022-12-13 20:36:09 -0300

    tentativas recycler view

--- app/src/main/java/com/evavictoria/locus/guidetour/RotasFragment.java
+++ app/src/main/java/com/evavictoria/locus/guidetour/RotasFragment.java
@@ -8,33 +8,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.evavictoria.locus.guidetour.databinding.FragmentRotasBinding;
 import com.evavictoria.locus.guidetour.model.PontoTuristico;
+import com.evavictoria.locus.guidetour.service.ConfiguracaoFirebase;
 import com.evavictoria.locus.guidetour.service.GuideTourRepository;
 import com.evavictoria.locus.guidetour.viewmodel.RotasViewModel;
 import com.evavictoria.locus.guidetour.views.PontoTuristicoAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class RotasFragment extends Fragment {
 
-    FragmentRotasBinding binding;
+   // FragmentRotasBinding binding;
     List<PontoTuristico> pontosTuristicosList;
     RecyclerView recyclerView;
     PontoTuristicoAdapter pontoTuristicoAdapter;
     DatabaseReference databaseReference;
+    ConfiguracaoFirebase configuracaoFirebase = new ConfiguracaoFirebase();
     RotasViewModel viewModel;
     GuideTourRepository guideTourRepository = new GuideTourRepository().getInstance();
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding = FragmentRotasBinding.bind(view);
+    //    binding = FragmentRotasBinding.bind(view);
         // viewModel.recuperarDados();
 
     }
@@ -43,7 +48,8 @@ public class RotasFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+       // recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 //        pontosTuristicosList = new ArrayList<>();
 //        databaseReference.child("pontosTuristicos").addListenerForSingleValueEvent(new ValueEventListener() {
 //            @Override
@@ -61,17 +67,37 @@ public class RotasFragment extends Fragment {
 //
 //            }
 //        });
-        viewModel = new ViewModelProvider(this).get(RotasViewModel.class);
-        viewModel.recuperarDados().observe(this, pontoTuristicos -> {
-            binding.nomePontoTuristicoRotas.setText(viewModel.retornaNome());
-            binding.descricaoPontoTuristicoRotas.setText(viewModel.retornaDescricao());
-        });
+//        viewModel = new ViewModelProvider(this).get(RotasViewModel.class);
+//        viewModel.recuperarDados().observe(this, pontoTuristicos -> {
+//            binding.nomePontoTuristicoRotas.setText(viewModel.retornaNome());
+//            binding.descricaoPontoTuristicoRotas.setText(viewModel.retornaDescricao());
+//        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rotas, container, false);
+        View view = inflater.inflate(R.layout.fragment_rotas, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        pontosTuristicosList = new ArrayList<>();
+        configuracaoFirebase.getFirebaseDatabase().child("pontosTuristicos").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dn:snapshot.getChildren()){
+                    PontoTuristico p = dn.getValue(PontoTuristico.class);
+                    pontosTuristicosList.add(p);
+                }
+                pontoTuristicoAdapter = new PontoTuristicoAdapter(pontosTuristicosList);
+                recyclerView.setAdapter(pontoTuristicoAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
     }
 }
